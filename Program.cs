24e1cf942c92a9b3184298d4562163e06d0d5aa9
Brian Kiefer <bakief04@gmail.com>
2021-03-08 18:38:52 -0500

    moved api to a diffewrent file and updated readme

--- Program.cs
+++ Program.cs
@@ -13,17 +13,15 @@ namespace Twitterbot
 {
     class Program
     {
-        private static string api_key = "UBVfuWPFvlhGJC3yCq2HY8Z36";
-        private static string api_key_secret = "Oz1XHOYSIAUuvenvR9o2RIGEVlcSK6y7PFUYddTpRb5ZR28bRd";
-        private static string access_token = "1367626134154842115-mHw8G1hFolmUtVlJkNXlBCiIWNkNZs";
-        private static string access_token_secret = "WHFaM0VDsMRW2UBEQM7JrsjnS80mihASd9d1sqkJYK9Sr";
+        
 
-        private static TwitterService service = new TwitterService(api_key, api_key_secret, access_token, access_token_secret);
+        private static TwitterService service = new TwitterService(Apikeys.api_key, Apikeys.api_key_secret, Apikeys.access_token, Apikeys.access_token_secret);
+
 
         static void Main(string[] args)
         {
             Console.WriteLine($"<{DateTime.Now}> - Bot Started");
-            SendTweet("It's Alive");
+            SendTweet("new api");
             Console.Read();
         }
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is my project. It's a twitter bot that uses the twitter api and a dictionary to randomly generate tweets from the text in a dictionary every time the code is ran.# Twitterbot
+This is my project. It's a twitter bot that uses the twitter api and a dictionary to randomly generate tweets from the text in a dictionary every time the code is ran. To get the Api information. Message Brian Kiefer on slack.
