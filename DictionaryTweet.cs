ec5aa521390edcdc9246354c7d595b9ee8aa311b
Brian Kiefer <bakief04@gmail.com>
2021-03-15 18:13:16 -0400

    Added Sample dictionary

---
+++ DictionaryTweet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twitterbot
+{
+    static class DictionaryTweet
+    {
+        public static Dictionary<int, string> TweetList = new Dictionary<int, string>
+        {
+            {1, "hello world" },
+            {2, "twitter is cool" },
+            {3, "this is for testing purposes" }
+        };
+    }
+}
